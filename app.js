8cb1bf43850932cf3cc93f004d5b205f0b8124bb
sadiq <sadiq7753@gmail.com>
2015-05-21 01:08:34 +0500

    added socket

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
 var app = require('express')();
 var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var socketio = require('socket.io').listen(server);
+var roomName = "MegaTransfer";
 
 server.listen(80);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.send('Hello, Welcome to MegaTransfer. Please use android and not browser. You stupid.');
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+socketio.on('connection', function (socket) {
+
+
+	var id = (Math.random().toString(10) + '0000000000000000000').substr(2, 4);
+
+	socket.set("username", id);
+	socket.join(roomName);
+	socket.emit('id', id);
+	
+
+	socket.on('message', function (message) {
+
+		var clients = socketio.sockets.clients(message.room);
+
+		var socketid = '';
+
+		var i = 0;
+		clients.forEach(function(client) {
+			client.get('username', function(err, nickname) {
+				if(nickname == message.to)
+					socketid = client.id;
+				i++;
+			})
+		});
+
+
+      var msgToSend = message.msg;
+
+      if(typeof msgToSend != 'object')
+        msgToSend = JSON.parse(msgToSend);
+
+		socketio.sockets.socket(socketid).emit('message', msgToSend);
+
+	});
+
+		
 });
